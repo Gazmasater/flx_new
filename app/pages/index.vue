71871535b98c3e05eab90940c63412eb2b92f517
Lev <tolkoplus@gmail.com>
2026-06-09 02:44:20 +0300

    add polit

---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { company } from "../data/company";
+import { advantages, services, steps } from "../data/landing";
+</script>
+
+<template>
+  <div class="bg-slate-950 text-white min-h-screen">
+    <SiteHeader :company="company" />
+
+    <main>
+      <HeroSection :company="company" />
+      <ServicesSection :services="services" />
+      <ProcessSection :steps="steps" />
+      <AdvantagesSection :advantages="advantages" />
+      <ContactsSection :company="company" />
+
+      <LeadForm />
+    </main>
+
+    <SiteFooter :company="company" />
+  </div>
+</template>
